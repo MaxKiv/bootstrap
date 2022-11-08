17d3b0fa39e1be56dbc6a0c305db989f1412731c
Max Kivits <101753609+MaxKiv@users.noreply.github.com>
2022-11-08 07:44:06 -0800

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,3 @@
+iwr -outf C:\Users\max\install-nerdfonts.ps1 https://gist.githubusercontent.com/daretodave/bd08f981ea51240053cfcf073272de25/raw/4ab599950e8e323f21c8c261bb25b210f89bcc55/install-nerdfonts.ps1
+cd C:\Users\max
+.\install-nerdfonts.ps1
