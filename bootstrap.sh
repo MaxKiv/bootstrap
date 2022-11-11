09580306a57be23befd326e1ec818e2a201209bb
Max Kivits <maxkivits42@gmail.com>
2022-11-11 09:40:39 +0100

    added fd and ripgrep

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,26 @@
 echo "Bootstrapping system"
 
-echo "Detected Debian UNIX"
+echo "Detected Ubuntu"
 
+echo "Downloading apt packages"
 # apt
 sudo apt update && sudo apt upgrade -y
+# font stuff
 sudo apt install fontconfig
+# unzip
 sudo apt install unzip
+# fd
+sudo apt install fd-find
+# ripgrep
+sudo apt-get install ripgrep
 
+echo "Downloading Hasklig font"
 # font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.0-RC/Hasklig.zip
 unzip Hasklig.zip -d ~/.fonts
 fc-cache -fv
 
+echo "Seting up Tmux"
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
@@ -28,7 +37,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/git/fzf
 ~/git/fzf/install
 
 # neovim
-echo "Neovim"
+echo "Installing Neovim"
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt-get install neovim
