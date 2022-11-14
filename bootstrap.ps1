a07afe50fa6c41c8210e34342e5282b759355728
Max Kivits <116153361+Max-Kivits@users.noreply.github.com>
2022-11-14 01:08:14 -0800

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,7 +7,7 @@ echo "TODO"
 
 echo "Installing fonts"
 iwr -outf C:\Users\max\install-nerdfonts.ps1 https://gist.githubusercontent.com/daretodave/bd08f981ea51240053cfcf073272de25/raw/4ab599950e8e323f21c8c261bb25b210f89bcc55/install-nerdfonts.ps1
-cd C:\Users\max
+cd $HOME\max
 .\install-nerdfonts.ps1
 
 echo "Downloading apt packages"
